e86c789d83fa4b24c17d9f62ec55451e6cbf1db8
Ahmed Elnageh <50675185+MarcoPolio765@users.noreply.github.com>
2025-12-16 16:36:39 +0000

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -1,25 +1,32 @@
 import numpy as np
 
-password = 0
 angle = 50
+password = 0
 rotations = np.loadtxt('rotations.txt', dtype=str)
 
 for line in rotations:
     direction = ""
     amount = ""
-    while i := 0 < len(line):
-        if line[i].isalpha():
-            direction += line[i]
-        elif line[i].isdigit():
-            amount += line[i]
+    for char in line:
+        if char.isalpha():
+            direction += char
+        elif char.isdigit():
+            amount += char
     amount = int(amount)
     if direction == "L":
         angle -= amount
     elif direction == "R":
         angle += amount
-    
-    if angle % 100 ==0:
+
+    while angle > 99:
+            angle -= 100
+            password += 1 # part two
+
+    while angle < 0:
+            angle += 100
+            password += 1 # part two
+
+    if angle == 0:
         password += 1
 
 print(password)
-
